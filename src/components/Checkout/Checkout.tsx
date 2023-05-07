5be0ea80c7da9e19c6f3b4f4ccff5b6b71b17892
Mahadeo Tonde <mahadeo16@gmail.com>
2023-05-07 22:22:34 +0530

    Add Links to Card

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -1,16 +1,17 @@
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 const Checkout = () => {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Shopping card</Card.Title>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
         <Card.Text>
-          Please use this form to check out the details about the products
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Checkout</Button>
+        <Card.Link href="/Home">Home</Card.Link>
+        <Card.Link href="/login">login</Card.Link>
       </Card.Body>
     </Card>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
       <Navbar.Brand href="/">Home</Navbar.Brand>
       <Nav className="me-auto">
         <Nav.Link href="/login">Login</Nav.Link>
-        <Nav.Link href="/about-us">Checkout</Nav.Link>
+        <Nav.Link href="/checkout">Checkout</Nav.Link>
         <Nav.Link href="/register">Registration</Nav.Link>
       </Nav>
     </Container>
